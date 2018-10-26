5b07605e927a94f50d61127c6a96b3d0d1f9676a
dianasifgudm <diana--sif@hotmail.com>
2018-10-26 13:19:58 +0000

    p0

--- P0-HalloHeimur/sketch.js
+++ P0-HalloHeimur/sketch.js
@@ -1,1 +1,11 @@
 // Hér kemur kóðinn þinn:
+function setup(){
+    createCanvas(500,500);
+    background(444,5,9);
+    fill(50,100);
+    ellipse(100,100,50,50);
+    fill(0);
+    textSize(40);
+    text("Halló kall",150,50);
+
+}
