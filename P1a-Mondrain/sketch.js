ed1fce5fe255effb6feb5fc9f3747717d485ed66
dianasifgudm <diana--sif@hotmail.com>
2018-10-26 14:00:26 +0000

    P0 klárað

--- P1a-Mondrain/sketch.js
+++ P1a-Mondrain/sketch.js
@@ -4,12 +4,9 @@ function setup(){
     background(444,5,9);
     fill(0,255,0);
     ellipse(100,100,50,50);
-    fill(51);
+    fill(50);
     textSize(40);
     text("Halló kalli",150,50);
-    fill(color(0,0,255));
     rect(200,200,50,50);
-    fill('#fae');
     ellipse(100,200,50,50);
-
 }
