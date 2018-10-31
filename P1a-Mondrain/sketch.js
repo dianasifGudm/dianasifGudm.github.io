7b4778beac54364ea2013706c7e99cb0eb50a146
dianasifgudm <diana--sif@hotmail.com>
2018-10-31 09:13:00 +0000

    p1a klárað

--- P1a-Mondrain/sketch.js
+++ P1a-Mondrain/sketch.js
@@ -1,12 +1,37 @@
 // Hér kemur kóðinn þinn:
 function setup(){
-    createCanvas(500,500);
-    background(444,5,9);
-    fill(0,255,0);
-    ellipse(100,100,50,50);
-    fill(50);
-    textSize(40);
-    text("Halló kalli",150,50);
-    rect(200,200,50,50);
-    ellipse(100,200,50,50);
+  createCanvas(333,328);
+  background(233,231,237);
+  strokeWeight(5);
+  stroke(51);
+  fill(211,0,0);
+  rect(-5,-5,150,190);
+  strokeWeight(5);
+  stroke(51);
+  fill(236,236,236);
+  rect(145,-5,190,190);
+  strokeWeight(5);
+  stroke(51);
+  fill(232,234,228);
+  rect(-5,185,150,145);
+  strokeWeight(5);
+  stroke(51);
+  fill(235,239,227);
+  rect(145,185,160,130);
+  strokeWeight(5);
+  stroke(51);
+  fill(249,216,6);
+  rect(145,315,100,15);
+  strokeWeight(5);
+  stroke(51);
+  fill(34,66,138);
+  rect(305,185,30,60);
+  strokeWeight(5);
+  stroke(51);
+  fill(228,234,224);
+  rect(305,245,30,85);
+  strokeWeight(5);
+  stroke(51);
+  fill(35,34,37);
+  rect(235,315,70,15);
 }
