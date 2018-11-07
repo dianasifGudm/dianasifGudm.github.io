93b2684bbfa27833259f69b76fe14d2c90dba168
dianasifgudm <diana--sif@hotmail.com>
2018-11-07 21:51:50 +0000

    P2 klárað

--- P2-Andlit/sketch.js
+++ P2-Andlit/sketch.js
@@ -1,365 +1,95 @@
-// Hér kemur kóðinn þinn:
+//breytur
+var breidd_andlits = 100;
+var haed_andlits  = 150;
+var litur_andlits_raudur = "200"
+var litur_andlits_graenn = "100"
+var litur_andlits_blar = "200"
+var haed_auga = 20
+var breidd_auga= 30
+var x_auga = 0
+var y_auga = 0
+var litur_auga = 180
+var haed_munnur = 10
+var breidd_munnur= 50
+var litur_munnur = 100
+
+var x = 150
+var y = 150
+var hopp = 1
+var Klikk = false
+
 function setup(){
-  createCanvas(400,495);
-  background(233,231,237);
+  //teiknisvæði
+  createCanvas (300,300);
+}
 
-  //rammi
-
-  strokeWeight(0);
-  stroke(0);
-  fill(98, 57, 42);
-  rect(28,28,333,333);
-
-  strokeWeight(0);
-  stroke(0);
+function draw(){
+  //bakgrunnur
+  background(100);
+  //andlit
+  fill(litur_andlits_raudur, litur_andlits_graenn,litur_andlits_blar);
+  ellipse(x, y, breidd_andlits, haed_andlits);
+  //augu.  x_auga og y_auga hreyfa augu
+  fill(255,255,255);
+  ellipse(x-25, y-20, breidd_auga, haed_auga);
+  ellipse(x+25, y-20, breidd_auga, haed_auga);
+  fill(litur_andlits_raudur, 130,100);
+  ellipse(x-25+x_auga, y-20+y_auga, breidd_auga*0.5, haed_auga*0.5);
+  ellipse(x+25+x_auga, y-20+y_auga, breidd_auga*0.5, haed_auga*0.5);
   fill(0,0,0);
-  rect(33,33,323,323);
-
-//fyrsta röð frá vinstri (blár)
-  strokeWeight(0);
-  stroke(0);
-  fill(27,34,133);
-  rect(35,35,40,40);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(167,0,0);
-  rect(75,35,40,40);
-
-  fill(174,0,0);
-  triangle(115,35, 115,75, 155,35);
-
-  fill(0,110,72);
-  triangle(155,75, 115,75, 155,35);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(0,110,76);
-  rect(155,35,40,40);
-
-  fill(33,18,26);
-  triangle(195,35, 195,75, 235,35);
-
-  fill(233,141,4);
-  triangle(235,75, 195,75, 235,35);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(231,137,3);
-  rect(235,35,40,40);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(152,0,0);
-  rect(275,35,40,40);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(27,39,130);
-  rect(315,35,40,40);
-//Önnur röd frá vinstri (svartur)
-  strokeWeight(0);
-  stroke(0);
-  fill(29,14,21);
-  rect(35,75,40,40);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(152,0,0);
-  rect(75,75,40,40);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(35,21,26);
-  rect(115,75,40,40);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(34,18,26);
-  rect(155,75,40,40);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(31,16,24);
-  rect(195,75,40,40);
-
-  fill(234,135,0);
-  triangle(235,75, 235,115, 275,75);
-
-  fill(148,0,4);
-  triangle(275,115, 235,115, 275,75);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(153,0,0);
-  rect(275,75,40,40);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(119,86,63);
-  rect(315,75,40,40);
-// þriðja röð
-  strokeWeight(0);
-  stroke(0);
-  fill(30,15,23);
-  rect(35,115,40,40);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(149,0,0);
-  rect(75,115,40,40);
-
-  fill(31,16,26);
-  triangle(115,115, 115,155, 155,115);
-
-  fill(231,135,0);
-  triangle(235,155, 115,155, 155,115);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(32,17,25);
-  rect(155,115,40,40);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(32,17,25);
-  rect(195,115,40,40);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(166,0,0);
-  rect(235,115,40,40);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(153,0,0);
-  rect(275,115,40,40);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(119,86,63);
-  rect(315,115,40,40);
-
-  //fjórða röð
-  strokeWeight(0);
-  stroke(0);
-  fill(28,13,21);
-  rect(35,155,40,40);
-
-  fill(232,134,0);
-  triangle(115,195, 75,195, 115,155);
-
-  fill(230,213,0);
-  triangle(115,155, 75,195, 75,155);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(230,137,0);
-  rect(115,155,40,40);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(31,16,24);
-  rect(155,155,40,40);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(161,0,0);
-  rect(195,155,40,40);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(158,0,0);
-  rect(235,155,40,40);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(121,90,68);
-  rect(275,155,40,40);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(125,93,70);
-  rect(315,155,40,40);
-
-  //5 röð
-  strokeWeight(0);
-  stroke(0);
-  fill(27,10,19);
-  rect(35,195,40,40);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(227,129,0);
-  rect(75,195,40,40);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(232,237,247);
-  rect(115,195,40,40);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(223,233,243);
-  rect(155,195,40,40);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(162,0,0);
-  rect(195,195,40,40);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(167,0,0);
-  rect(235,195,40,40);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(127,95,74);
-  rect(275,195,40,40);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(224,228,238);
-  rect(315,195,40,40);
-
-  //6. röð
-  strokeWeight(0);
-  stroke(0);
-  fill(222,230,241);
-  rect(35,235,40,40);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(199,0,0);
-  rect(75,235,40,40);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(226,227,241);
-  rect(115,235,40,40);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(235,239,240);
-  rect(155,235,40,40);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(232,237,244);
-  rect(195,235,40,40);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(195,0,0);
-  rect(235,235,40,40);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(181,0,0);
-  rect(275,235,40,40);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(216,220,230);
-  rect(315,235,40,40);
-
-  //7. röð
-  strokeWeight(0);
-  stroke(0);
-  fill(216,224,235);
-  rect(35,275,40,40);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(186,0,0);
-  rect(75,275,40,40);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(228,233,237);
-  rect(115,275,40,40);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(233,233,233);
-  rect(155,275,40,40);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(227,232,233);
-  rect(195,275,40,40);
-
-  fill(221,221,239);
-  triangle(235,275, 275,275, 275,315);
-
-  fill(241,215,0);
-  triangle(235,315, 235,275, 275,315);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(180,0,0);
-  rect(275,275,40,40);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(218,221,228);
-  rect(315,275,40,40);
-
-//8. röð og neðst niðri
-  strokeWeight(0);
-  stroke(0);
-  fill(18,50,146);
-  rect(35,315,40,40);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(178,0,0);
-  rect(75,315,40,40);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(226,204,0);
-  rect(115,315,40,40);
-
-  fill(222,206,0);
-  triangle(155,355, 155,315, 195,315);
-
-  fill(0,165,59);
-  triangle(155,355, 195,355, 195,315);
-
-
-  fill(238,213,0);
-  triangle(195,315, 235,315, 235,355);
-
-  fill(213,223,233);
-  triangle(235,355, 195,355, 195,315);
-
-
-  strokeWeight(0);
-  stroke(0);
-  fill(233,208,0);
-  rect(235,315,40,40);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(194,0,0);
-  rect(275,315,40,40);
-
-  strokeWeight(0);
-  stroke(0);
-  fill(21,45,146);
-  rect(315,315,40,40);
-
-//////////////////////////
-//for lykkja testuð
-
-//var i, x, y;
-//for (i = 0; i < 60; i++) {
-//  rect(i*10,i*10,i*10,i*10);
-//  fill(240,i*6,i*11);
-//}
-//////////////////////////
-
+  ellipse(x-25+x_auga, y-20+y_auga, breidd_auga*0.2, haed_auga*0.2);
+  ellipse(x+25+x_auga, y-20+y_auga, breidd_auga*0.2, haed_auga*0.2);
+  //skegg
+  triangle(x-breidd_munnur/2,y+30-haed_munnur/2, x+breidd_munnur/2,y+30-haed_munnur/2, x,y+25-haed_munnur/2);
+  triangle(x-breidd_munnur/5,y+30+haed_munnur/2, x+breidd_munnur/5,y+30+haed_munnur/2, x,y+haed_andlits/3+5);
+  //munnur
+  fill(255,0,0);
+  ellipse(x, y+30, breidd_munnur, haed_munnur);
+
+
+
+
+  //.....kóði sem staðsetur augun m.v. x, y, faceWidth og faceHeight ætti að koma hér.
+
+}
+
+function mouseClicked(){
+  //Í hvert sinn sem músinni er smellt fá breyturnar ný gildi.
+  x = 150;
+  y = 150;
+  haed_andlits   = random(95,  250);
+  breidd_andlits  = random(120, 250);
+  litur_andlits_raudur = random(100, 250)
+  litur_andlits_graenn = random(100, 250)
+  litur_andlits_blar = random(100, 250)
+  haed_auga    = random(10,  40);
+  breidd_auga    = random(10,  40);
+  haed_munnur    = random(5,  15);
+  breidd_munnur    = random(10,  breidd_andlits-70);
+
+  if (hopp == 1){
+    y=haed_andlits/2;
+    x_auga = breidd_auga/random(4,  breidd_auga);
+    y_auga = haed_auga/random(4,  haed_auga);
+    hopp=2
+  } else {
+    y=300-haed_andlits/2
+    x_auga = -breidd_auga/random(4,  breidd_auga);
+    y_auga = -haed_auga/random(4,  haed_auga);
+
+    hopp =1
+    Klikk = true
+  }
+}
+function mouseMoved(){
+    x_auga = 0;
+    y_auga = 0;
+
+if (Klikk == true){
+  Klikk = false
+  haed_auga =haed_auga +5;
+  breidd_auga =breidd_auga +5;
+  breidd_munnur = breidd_munnur+20
+}
 
 }
